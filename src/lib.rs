ea1d9373c6c97fbb7271a24833361bde9df9e964
yinpeach <yinpeach0314@gmail.com>
2024-04-05 09:39:56 +0800

    modified:   src/lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -3,25 +3,25 @@ use std::{sync::{Arc, RwLock}, time::{Duration, Instant}};
 
 #[derive(Debug, Clone)]
 pub struct Timer {
-    timer: Arc<RwLock<Instant>>
+    timer: Instant
 }
 
 impl Default for Timer {
     fn default() -> Self {
-        Self { timer: Arc::new(RwLock::new(Instant::now())) }
+        Self { timer: Instant::now() }
     }
 }
 
 impl Timer {
     pub fn new() -> Self {
-        Self { timer: Arc::new(RwLock::new(Instant::now())) }
+        Self { timer: Instant::now() }
     }
 
-    pub fn reset(&self) {
-        *self.timer.write().unwrap() = Instant::now();
+    pub fn reset(&mut self) {
+        self.timer = Instant::now();
     }
 
     pub fn timer(&self) -> Duration {
-        Instant::now().duration_since(*self.timer.read().unwrap())
+        Instant::now().duration_since(self.timer)
     }
 }
